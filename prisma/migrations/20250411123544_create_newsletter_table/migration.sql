b14639596549d0db8c3f7deeddd0244eec4651cc
Eduardo A. Pacheco <epacheco.dev@gmail.com>
2025-04-11 10:06:43 -0300

    feat: create newsletter subscription endpoint

--- prisma/migrations/20250411123544_create_newsletter_table/migration.sql
+++ prisma/migrations/20250411123544_create_newsletter_table/migration.sql
@@ -1,9 +1,9 @@
 -- CreateTable
-CREATE TABLE "newslatter" (
+CREATE TABLE "newsletter" (
     "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     "email" TEXT NOT NULL,
     "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 -- CreateIndex
-CREATE UNIQUE INDEX "newslatter_email_key" ON "newslatter"("email");
+CREATE UNIQUE INDEX "newsletter_email_key" ON "newsletter"("email");
---
+++ src/app/api/v1/newsletter/route.ts
@@ -0,0 +1,37 @@
+import { NextRequest, NextResponse } from 'next/server';
+import { Newsletter, newsletterSchema } from '@/libs/api/schemas/newsletter-schema';
+import { ZodError } from 'zod';
+import { db } from '@/libs/database/db';
+
+export async function POST(req: NextRequest) {
+  try {
+    const response: Newsletter = await req.json();
+    const newsletter: Newsletter = newsletterSchema.parse(response);
+
+    const findNewsletter = await db.newsletter.findUnique({
+      where: {
+        email: newsletter.email,
+      },
+    });
+
+    if (findNewsletter)
+      return NextResponse.json({ message: 'User Already Registered' }, { status: 409 });
+
+    const registerNewsletter = await db.newsletter.create({
+      data: {
+        email: newsletter.email,
+      },
+    });
+
+    return NextResponse.json(
+      { message: 'Successfully Registered', data: registerNewsletter },
+      { status: 200 },
+    );
+  } catch (err) {
+    console.error(err);
+    if (err instanceof ZodError) {
+      return NextResponse.json({ message: err }, { status: 500 });
+    }
+    return NextResponse.json({ message: 'Internal Server Error' }, { status: 500 });
+  }
+}
