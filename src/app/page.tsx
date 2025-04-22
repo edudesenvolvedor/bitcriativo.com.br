05ddcc2ec49456a1e06527a4a377aac7cf2d2d36
Eduardo Pacheco <epacheco.pessoal@gmail.com>
2025-04-22 09:01:05 -0300

    chore: rename hero section to follow kebab case convention

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import { Hero } from '@/components/section/Hero';
+import { Hero } from '@/components/section/hero';
 import { About } from '@/components/section/about';
 import { Features } from '@/components/section/features';
 import { Contact } from '@/components/section/contact';
