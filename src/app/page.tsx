c08494645148752c10cc9b34becd9eb2fef4e725
Eduardo Pacheco <epacheco.pessoal@gmail.com>
2025-04-22 08:46:40 -0300

    chore: rename main-layout component to follow kebab case convention

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { Hero } from '@/components/section/Hero';
 import { About } from '@/components/section/About';
 import { Features } from '@/components/section/Features';
 import { Contact } from '@/components/section/contact';
-import { MainLayout } from '@/layouts/MainLayout';
+import { MainLayout } from '@/layouts/main-layout';
 import { Portfolio } from '@/components/section/Portfolio';
 import { Pricing } from '@/components/section/pricing';
 import { Testimonials } from '@/components/section/Testimonials';
